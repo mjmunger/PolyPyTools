089114730776837468502b04736fd223f0baf2b0
Michael Munger <michael@buster.hph.io>
2020-04-14 22:40:54 -0400

    Added polypy site setup and configs

--- poly_py_tools/polypy_site.py
+++ poly_py_tools/polypy_site.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """
-Usage: polypy site [ -v ... ] setup <site>
+Usage: polypy site [ options ] setup sntp for <site> --offset=<gmtoffset> [--server=<ntp_server> ]
+       polypy site [ options ] setup syslog for <site> [ --server=<syslog_server> ]
+       polypy site [ options ] setup nat for <site> --ip=<ip> [ --mediaPortStart=<mediaPortStart> --signalPort=<signalPort> ]
+       polypy site [ options ] setup password for <site> to <password>
+       polypy site [ options ] setup digitmap for <site> add <pattern>
+       polypy site [ options ] setup digitmap for <site> del <pattern>
+       polypy site [ options ] setup voipprot for <site> --address=<address> [ --port=<port> ]
+       polypy site [ options ] configs flush
+       polypy site [ options ] configs fill
 
   Commands:
       setup        Setup a site with basic settings.
+      flush        Remove all the configured cfg files (but not the ones from the Config/ directory)
 
   Options:
     -d,            Debug mode
@@ -25,13 +34,46 @@ from shutil import copyfile
 
 args = docopt(__doc__)
 
+if args['-d']:
+    print("-----DEBUG MODE-----")
+    print(args)
+
 config_finder = ConfigFinder()
 configs = config_finder.get_configs()
 
-print(args)
-
 writer = SiteWriter(configs)
+writer.debug_mode = args['-d']
+writer.set_site(args['<site>'])
 
-if args['setup'] and args['site']:
-    writer.set_site(args['<site>'])
-    writer.setup_all()
+if args['setup'] and args['syslog']:
+    writer.syslog_server = "pbx.hph.io" if args['--server'] is None else args['--server']
+    writer.setup_syslog()
+
+if args['setup'] and args['sntp']:
+    writer.set_gmt_offset(args['--offset'])
+    writer.ntp_server = "0.north-america.pool.ntp.org" if args['--server'] is None else args['--server']
+    writer.setup_sntp()
+
+if args['setup'] and args['nat']:
+    writer.nat_ip = args['--ip']
+    writer.nat_media_start_port = "20000" if args['--mediaPortStart'] is None else args['--mediaPortStart']
+    writer.nat_signal_port = "5060" if args['--signalPort'] is None else args['--signalPort']
+    writer.setup_nat()
+
+if args['setup'] and args['password']:
+    writer.setup_local_admin_password(args['<password>'])
+
+if args['setup'] and args['digitmap'] and args['add']:
+    writer.add_digitmap(args['<pattern>'])
+
+if args['setup'] and args['digitmap'] and args['del']:
+    writer.del_digitmap(args['<pattern>'])
+
+if args['setup'] and args['voipprot']:
+    writer.setup_voipprot(args['--address'], "5060" if args['--port'] is None else args['--port'])
+
+if args['configs'] and args['flush']:
+    writer.flush_cfgs()
+
+if args['configs'] and args['fill']:
+    writer.fill_cfgs()
--- poly_py_tools/site_writer.py
+++ poly_py_tools/site_writer.py
@@ -1,6 +1,9 @@
 import xml
 import xml.dom.minidom
 import os
+import sys
+from pprint import pprint
+from shutil import copy
 
 
 class SiteWriter:
@@ -10,20 +13,24 @@ class SiteWriter:
     source_paths = {}
     dst_paths = {}
     site = None
+    gmtOffset = None
+    debug_mode = False
+    syslog_server = "pbx.hph.io"
+    ntp_server = "0.north-america.pool.ntp.org"
+    nat_ip = None
+    nat_media_start_port = None
+    nat_signal_port = None
 
     def __init__(self, configs):
         self.configs = configs
-        print(configs)
         self.source_paths = {"device.cfg" : None, "features.cfg" : None, "H323.cfg" : None, "reg-advanced.cfg" : None,
                              "reg-basic.cfg" : None, "region.cfg": None, "sip-basic.cfg": None, "sip-interop.cfg": None,
                              "site.cfg" : None, "video.cfg" : None, "video-integration.cfg" : None}
 
-        self.dst_paths = self.source_paths
+        self.dst_paths = self.source_paths.copy()
 
     def setup_paths(self):
-        print(self.configs['paths']['tftproot'])
         for k in self.source_paths:
-            print(self.source_paths[k])
             self.source_paths[k] = os.path.join(os.path.join(self.configs['paths']['tftproot'], "Config"), k)
 
         for k in self.dst_paths:
@@ -33,27 +40,84 @@ class SiteWriter:
         self.site = site
         self.setup_paths()
 
-    def setup_site_cfg(self):
-        sitecfg = xml.dom.minidom.parse(self.source_paths['site.cfg'])
-        root = sitecfg.getElementsByTagName('polycomConfig')
+    def set_gmt_offset(self, gmtOffset):
+        # self.gmtOffset = str(int(gmtOffset) * -1)
+        self.gmtOffset = gmtOffset
+        if self.debug_mode:
+            print("gmtOffset set to: {}".format(self.gmtOffset))
 
-        #Enable syslog
-        syslog_node = root[0].getElementsByTagName('device.syslog')
+    def get_cfg(self, file):
+        return self.dst_paths[file] if os.path.exists(self.dst_paths[file]) else \
+            self.source_paths[file]
+
+    def setup_site_cfg(self):
+        root, syslog_node = self.setup_syslog()
+
+        if self.debug_mode:
+            print(syslog_node.toxml())
+
+        sntp_node = self.setup_sntp(root)
+
+        if self.debug_mode:
+            print("SNTP:")
+            print(sntp_node.toxml())
+
+    def setup_syslog(self):
+
+        sitecfg = xml.dom.minidom.parse(self.get_cfg('site.cfg'))
+        root = sitecfg.getElementsByTagName('polycomConfig')[0]
+
+        syslog_node = root.getElementsByTagName('device.syslog')[0]
         syslog_node.setAttribute("device.syslog.prependMac", "1")
-        syslog_node.setAttribute("device.syslog.serverName", "pbx.hph.io")
+        syslog_node.setAttribute("device.syslog.serverName", self.syslog_server)
         syslog_node.setAttribute("device.syslog.transport", "UDP")
 
-        root.toxml()
+        device_syslog_prependMac = syslog_node.getElementsByTagName("device.syslog.prependMac")[0]
+        device_syslog_prependMac.setAttribute("device.syslog.prependMac.set", "1")
 
-    def setup_all(self):
+        device_syslog_serverName = syslog_node.getElementsByTagName("device.syslog.serverName")[0]
+        device_syslog_serverName.setAttribute("device.syslog.serverName.set", "1")
 
-        self.setup_site_cfg()
-        # self.enable_syslog()
-        # self.setup_digit_map()
-        # self.setup_ntp()
+        device_syslog_transport = syslog_node.getElementsByTagName("device.syslog.transport")[0]
+        device_syslog_transport.setAttribute("device.syslog.transport.set", "1")
+
+        self.save_cfg('site.cfg', sitecfg)
+
+    def setup_sntp(self):
+
+        sitecfg = xml.dom.minidom.parse(self.get_cfg('site.cfg'))
+        root = sitecfg.getElementsByTagName('polycomConfig')[0]
+
+        sntp_node = root.getElementsByTagName("device.sntp")[0]
+        sntp_node.setAttribute("device.sntp.gmtOffset", self.gmtOffset)
+        sntp_node.setAttribute("device.sntp.serverName", self.ntp_server)
+        sntp_node_gmtoffset = sntp_node.getElementsByTagName("device.sntp.gmtOffset")[0]
+        sntp_node_gmtoffset.setAttribute("device.sntp.gmtOffset.set", "1")
+        sntp_node_server_name = sntp_node.getElementsByTagName("device.sntp.serverName")[0]
+        sntp_node_server_name.setAttribute("device.sntp.serverName.set", "1")
+
+        self.save_cfg('site.cfg', sitecfg)
+
+    def save_cfg(self, cfg_file, xml_document):
+        fp = open(self.dst_paths[cfg_file], 'w')
+        fp.write(xml_document.toxml())
+        fp.close()
+
+    def setup_nat(self):
+
+        sip_interop = xml.dom.minidom.parse(self.get_cfg('sip-interop.cfg'))
+        root = sip_interop.getElementsByTagName('polycomConfig')[0]
+
+        nat_node = root.getElementsByTagName("nat")[0]
+        nat_node.setAttribute("nat.ip", self.nat_ip)
+        nat_node.setAttribute("nat.mediaStartPort", self.nat_media_start_port)
+        nat_node.setAttribute("nat.signalPort", self.nat_signal_port)
+
+        self.save_cfg('sip-interop.cfg', sip_interop)
 
     def __str__(self):
         buffer = []
+        buffer.append("Debug mode: {}".format("On" if self.debug_mode else "Off"))
         buffer.append("Paths")
         buffer.append("  Source Paths")
         for k in self.source_paths:
@@ -64,3 +128,68 @@ class SiteWriter:
             buffer.append("    {}: {}".format(k,self.dst_paths[k]))
 
         return "\n".join(buffer)
+
+    def setup_local_admin_password(self, password):
+        site_cfg = xml.dom.minidom.parse(self.get_cfg('site.cfg'))
+        root = site_cfg.getElementsByTagName('polycomConfig')[0]
+
+        device_node = root.getElementsByTagName("device")[0]
+
+        auth_node = device_node.getElementsByTagName("device.auth")[0]
+        auth_node.setAttribute("device.auth.localAdminPassword", password)
+
+        self.save_cfg("site.cfg", site_cfg)
+
+    def del_digitmap(self, pattern):
+        site_cfg = xml.dom.minidom.parse(self.get_cfg('site.cfg'))
+        root = site_cfg.getElementsByTagName('polycomConfig')[0]
+
+        dialplan_node = root.getElementsByTagName("dialplan")[0]
+        digitmap = dialplan_node.getAttribute("dialplan.digitmap").split("|")
+        digitmap.remove(pattern)
+        dialplan_node.setAttribute("dialplan.digitmap", "|".join(digitmap))
+
+        self.set_digitmap_timeouts(digitmap, root)
+
+        self.save_cfg("site.cfg", site_cfg)
+
+    def add_digitmap(self, pattern):
+        site_cfg = xml.dom.minidom.parse(self.get_cfg('site.cfg'))
+        root = site_cfg.getElementsByTagName('polycomConfig')[0]
+
+        dialplan_node = root.getElementsByTagName("dialplan")[0]
+        digitmap = dialplan_node.getAttribute("dialplan.digitmap").split("|")
+        digitmap.append(pattern)
+        dialplan_node.setAttribute("dialplan.digitmap", "|".join(digitmap))
+
+        self.set_digitmap_timeouts(digitmap, root)
+
+        self.save_cfg("site.cfg", site_cfg)
+
+    def set_digitmap_timeouts(self, digitmap, root):
+        timeouts = "|".join(["3"] * len(digitmap))
+        digitmap_node = root.getElementsByTagName("dialplan.digitmap")[0]
+        digitmap_node.setAttribute("dialplan.digitmap.timeOut", timeouts)
+
+    def setup_voipprot(self, address, port):
+        basic_cfg = xml.dom.minidom.parse(self.get_cfg('sip-basic.cfg'))
+        root = basic_cfg.getElementsByTagName('polycomConfig')[0]
+
+        prot_node = root.getElementsByTagName("voIpProt.server")[0]
+        prot_node.setAttribute("voIpProt.server.1.address", address)
+        prot_node.setAttribute("voIpProt.server.1.port", port)
+
+        self.save_cfg("sip-basic.cfg", basic_cfg)
+
+    def flush_cfgs(self):
+        for cfg in self.dst_paths:
+            print("Removing: {}".format(self.dst_paths[cfg]))
+            os.remove(self.dst_paths[cfg])
+
+        print("Done. Configs flushed.")
+
+    def fill_cfgs(self):
+        for cfg in self.dst_paths:
+            if not os.path.exists(self.dst_paths[cfg]):
+                copy(self.source_paths[cfg], self.dst_paths[cfg])
+                print("Filling: {}".format(cfg))
