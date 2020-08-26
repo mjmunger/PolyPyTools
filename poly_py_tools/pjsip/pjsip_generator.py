dcc58f2f7dd9f3fb3c3f65585a236d5dd26b1177
Michael Munger <michael@highpoweredhelp.com>
2020-08-26 19:07:16 -0400

    Fixed target path for generate. Was going to tftproot. Should be going to aserisk

--- poly_py_tools/pjsip/pjsip_generator.py
+++ poly_py_tools/pjsip/pjsip_generator.py
@@ -97,7 +97,7 @@ class PJSipGenerator(object):
 
     def run(self):
         self.generate_from(self.args['<file>'])
-        target_file = os.path.join(self.config.configs()['paths']['tftproot'], 'pjsip.conf')
+        target_file = os.path.join(self.config.configs()['paths']['asterisk'], 'pjsip.conf')
         f = open(target_file, 'w')
         f.write(self.conf())
         f.close()
